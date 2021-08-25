89f88e6412379d28e78ad4a8fd0e62779533bebc
DavidBlore <davidblore6555@gmail.com>
2021-08-25 09:53:15 +0200

    Refactor

--- src/CTImageSlice.java
+++ src/CTImageSlice.java
@@ -1,4 +1,5 @@
-/** A class representing a CT image slice object
+/** 
+ * A class representing a CT image slice object
  * @author SCTMIC015, SMTJUL022, BLRDAV002
  */
 
@@ -9,28 +10,47 @@ public class CTImageSlice {
     public int zCoOrd;
     public int xDimension;
     public int yDimension;
-    public int[][] imageArray;
+    private int[][] imageData;
     final int threshold = 120;   // Threshold for determining if a voxel is a fracture. To be determined analytically later
 
-    public CTImageSlice(int zCoOrd, int xDimension, int yDimension, int[][] imageArray){
+    /**
+     * Contructor when no image data has been extracted but image dimensions are known
+     * @param zCoOrd position in stack of images (i.e., The images Z co-ordinate)
+     * @param xDimension Width of the image
+     * @param yDimension Height of the image
+     */
+    public CTImageSlice(int zCoOrd, int xDimension, int yDimension){
         this.zCoOrd = zCoOrd;
         this.xDimension = xDimension;
         this.yDimension = yDimension;
-        this.imageArray = imageArray;
+        this.imageData = new int[xDimension][yDimension];
     }
 
     /**
-     * Finds the fracture voxels based on the colour of the voxel and the neighbour checking in testNeighbours method
+     * Contructor when image data has been extracted
+     * @param zCoOrd position in stack of images (i.e., The images Z co-ordinate)
+     * @param imageData Image data represented as a 2D array
+     */
+    public CTImageSlice(int zCoOrd, int[][] imageData){
+        this.zCoOrd = zCoOrd;
+        this.imageData = imageData;
+        // calculated from image data
+        this.xDimension = imageData.length;
+        this.yDimension = imageData.length;
+    }
+
+    /**
+     * Finds the fracture voxels by looking at the voxels colour and its neighbours
      * @return
      */
     public ArrayList<FractureVoxel> findFractureVoxels(){
         ArrayList<FractureVoxel> fractureVoxels = new ArrayList<FractureVoxel>();
-        for (int i = 1; i < xDimension -1; i ++){
-            for (int j = 1; j < yDimension -1; j ++){
-                if (imageArray[i][j] <= threshold && imageArray[i][j] > 0){
-                    fractureVoxels.add(new FractureVoxel(zCoOrd, j, i));
-                    //System.out.print(imageArray[i][j]);
-                    System.out.println("X-coOrd: " + j + " Y-coOrd: " + i);  // For some reason x and y are confused lol
+        for (int x = 1; x < xDimension -1; x ++){
+            for (int y = 1; y < yDimension -1; y ++){
+                if (imageData[x][y] <= threshold && imageData[x][y] > 0){
+                    fractureVoxels.add(new FractureVoxel(zCoOrd, y, x));
+                    //System.out.print(imageData[x][y]);
+                    System.out.println("X-coOrd: " + y + " Y-coOrd: " + x);  // For some reason x and y are confused lol
                 }
             }
             //System.out.println();
@@ -49,7 +69,7 @@ public class CTImageSlice {
         int count = 0;
         for (int i = x -1; i <= x +1; i ++){
             for (int j = y -1; j <= y +1; j ++){
-                if (imageArray[i][j] >threshold){
+                if (imageData[i][j] > threshold){
                     count ++;
                 }
             }
