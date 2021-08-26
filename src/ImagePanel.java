de8615d2e63c4f41e550578c326eb625005c80c6
DavidBlore <davidblore6555@gmail.com>
2021-08-26 21:09:36 +0200

    Implemented fracture colours

--- src/ImagePanel.java
+++ src/ImagePanel.java
@@ -4,32 +4,45 @@ import java.awt.image.*;
 import java.util.ArrayList;
 
 public class ImagePanel extends JPanel{
+    private static final int PANEL_SIZE = 600;
+    // Instance variables 
     private CTImageSlice imageSlice;
-    private ArrayList<FractureVoxel> fractureVoxels; // TODO: change to just fractures not fracture voxels
     private BufferedImage overlayImage;
+    private FractureCollection fractureCollection;
+    private ArrayList<FractureVoxel> fractureVoxels;
 
+    /**
+     * Basic constructor that intializes empty image panel
+     */
     public ImagePanel() {
         setupPanelBasics();
     }
 
+    /**
+     * Constructor for image panel that takes in an image slice
+     * This image slice is stored and rendered (through swing)
+     * @param imageSlice
+     */
     public ImagePanel(CTImageSlice imageSlice) {
         setupPanelBasics();
         this.imageSlice = imageSlice;
+        fractureVoxels = Dectection.findFractureVoxels(imageSlice); // detect fracture voxels 
     }
 
+    /**
+     * Sets up basic layout for image panel
+     */
     private void setupPanelBasics() {
-        // Set background colour
-        // this.setBackground(new java.awt.Color(0, 0, 204));
         // Setup layout
         javax.swing.GroupLayout imagePanelLayout = new javax.swing.GroupLayout(this);
         this.setLayout(imagePanelLayout);
         imagePanelLayout.setHorizontalGroup(
             imagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
+            .addGap(0, ImagePanel.PANEL_SIZE, Short.MAX_VALUE)
         );
         imagePanelLayout.setVerticalGroup(
             imagePanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
+            .addGap(0, ImagePanel.PANEL_SIZE, Short.MAX_VALUE)
         );
     }
     
@@ -38,18 +51,15 @@ public class ImagePanel extends JPanel{
      */
 	@Override
     protected void paintComponent(Graphics g) {
-		// int width = getWidth();
-		// int height = getHeight();
 		super.paintComponent(g);
         if (imageSlice != null) {
             // draw the image slice in greyscale as an image
             if (imageSlice.getImage() != null){
-                g.drawImage(imageSlice.getImage().getScaledInstance(600, 600, 0), 0, 0, null);
+                g.drawImage(imageSlice.getImage().getScaledInstance(ImagePanel.PANEL_SIZE, ImagePanel.PANEL_SIZE, 0), 0, 0, null);
             }
-            // TODO: paint fractures over image, using colour assigned to them
-            // change to use fractures not fractureVoxels
+            // draw the overlay image to show fracture colours 
             if (overlayImage != null) {
-                g.drawImage(overlayImage.getScaledInstance(600, 600, 0), 0, 0, null);
+                g.drawImage(overlayImage.getScaledInstance(ImagePanel.PANEL_SIZE, ImagePanel.PANEL_SIZE, 0), 0, 0, null);
             }
         }
 	}
@@ -60,24 +70,29 @@ public class ImagePanel extends JPanel{
      */
     public void setImageSlice(CTImageSlice imageSlice) {
         this.imageSlice = imageSlice;
+        fractureVoxels = Dectection.findFractureVoxels(imageSlice); // detect fracture voxels 
         repaint();
     }
 
     /**
-     * Dectects fractures on CTImageSlice and paints them red
+     * Dectects fractures on CTImageSlice and displays them
      */
     public void findFractures() {
         if (imageSlice != null) {
-            ArrayList<FractureVoxel> fractureVoxels = Dectection.findFractureVoxels(imageSlice); // detect fractures
-            overlayImage = new BufferedImage(imageSlice.getImage().getWidth(), imageSlice.getImage().getHeight(), BufferedImage.TYPE_INT_ARGB);
-            for (FractureVoxel fractureVoxel : fractureVoxels) {
-                int r = 255;
-                int g = 0;
-                int b = 0;
-                int a = 255;
-                Color color = new Color(r,g,b,a);
-                overlayImage.setRGB(fractureVoxel.getX(), fractureVoxel.getY(), color.getRGB()); // update
-            }
+            // Fracture collection 
+            fractureCollection = new FractureCollection(fractureVoxels);
+            overlayImage = fractureCollection.getImage(imageSlice.getZCoOrd(), imageSlice.getImage().getWidth(), imageSlice.getImage().getHeight()); // TODO: remove hardcoded values
+            repaint();
+        }
+    }
+
+    /**
+     * Adds colour to the different fractures
+     */
+    public void colorDifferentFractures() {
+        if (imageSlice != null) {
+            ColourBuilder.assignColorsToFractures(fractureCollection.getFractures(fractureVoxels));
+            overlayImage = fractureCollection.getImage(imageSlice.getZCoOrd(), imageSlice.getImage().getWidth(), imageSlice.getImage().getHeight()); // TODO: remove hardcoded values
             repaint();
         }
     }
