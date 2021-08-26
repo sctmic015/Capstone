80d37b0718dfa3bddd1494e5854ed31ce4d56100
DavidBlore <davidblore6555@gmail.com>
2021-08-26 11:02:54 +0200

    Neatened

--- src/Fracture.java
+++ src/Fracture.java
@@ -5,26 +5,41 @@ import java.util.ArrayList;
  * Fracture
  */
 public class Fracture {
-private ArrayList<FractureVoxel> arrVoxels = new ArrayList<FractureVoxel>(); 
+    private ArrayList<FractureVoxel> fractureVoxels = new ArrayList<FractureVoxel>(); 
+
+    /**
+     * Basic constructor
+     */
+    public Fracture() {}
 
 
-
-public void addVoxel(FractureVoxel v){
-    arrVoxels.add(v);
-}
-
-public int getSize(){
-    return arrVoxels.size();
-}
-
-public boolean isPartOfFracture(FractureVoxel f){
-    for (FractureVoxel i : arrVoxels){
-        if(i.checkNeighbourVoxel(f)){
-            return true;
+    /**
+     * Adds FractureVoxel to fracture and marks the voxel as assigned by referencing this fracture 
+     * @param fractureVoxel FractureVoxel to add to fracture
+     */
+    public void addVoxel(FractureVoxel fractureVoxel){
+        if(!fractureVoxels.contains(fractureVoxel)){
+            fractureVoxels.add(fractureVoxel);
         }
+        fractureVoxel.setAssignedFracture(this);
     }
-    return false;
-}
+
+    /**
+     * Gets number of fracture voxels in the fracture
+     * @return int number of fracture voxels in the fracture
+     */
+    public int getSize(){
+        return fractureVoxels.size();
+    }
+
+    /**
+     * Checks if given fracture voxel is in the fracture
+     * @param fractureVoxel Fracture voxel to look up 
+     * @return true if fracture contains the voxel, false otherwise
+     */
+    public boolean containsFractureVoxel(FractureVoxel fractureVoxelToCheck){
+        return fractureVoxels.contains(fractureVoxelToCheck);
+    }
 
     
 }
