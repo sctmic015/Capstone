c5edca94ecdb6ebdc8d520b4268c673fbb8cf531
DavidBlore <davidblore6555@gmail.com>
2021-08-31 20:03:29 +0200

    Intial

---
+++ src/PGMFilenameFilter.java
@@ -0,0 +1,34 @@
+import java.io.File;
+import javax.swing.filechooser.FileFilter;
+
+/**
+ * FileFilter for restricting user input to only select .pgm image files
+ */
+public class PGMFilenameFilter extends FileFilter {
+
+    /**
+     * Empty constructor
+     */
+    public PGMFilenameFilter() {}
+
+    /**
+     * Overides FileFilter interface's accept method 
+     * @param file the file that is being checked
+     * @return true if and only if the file ends with .pgm
+     */
+    @Override
+    public boolean accept(File file) {
+        return file.getName().toLowerCase().endsWith(".pgm");
+    }
+
+    /**
+     * Description to show in user file chooser 
+     * @return Description to show in user file chooser 
+     */
+    @Override
+    public String getDescription() {
+        return "Only .pgm";
+    }
+
+	
+}
