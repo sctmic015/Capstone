024b89338567f91a289aeb1341d8d26663eb1011
DavidBlore <davidblore6555@gmail.com>
2021-08-31 19:32:51 +0200

    Adjusted to take multiple files and only accept .pgm

--- src/FileOpener.java
+++ src/FileOpener.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.File;
+
 
 public class FileOpener extends JFrame{
 
@@ -8,19 +10,25 @@ public class FileOpener extends JFrame{
     }
 
 
-    public String getFileFromUser() {
+    /**
+     * Displays filechooser popup to user to let them load .pgm images 
+     * @return Array of Files selected by user 
+     */
+    public File[] getFilesFromUser() {
         // TODO: destory entire frame on close
         this.setVisible(true);
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setMultiSelectionEnabled(false); // allow selection of multiple files
-        // fileChooser.setFileFilter(new PDFFilter()); // only accept .pdf
-        fileChooser.setAcceptAllFileFilterUsed(false); // remove all files option
+        fileChooser.setFileFilter(new PGMFilenameFilter()); // only accept .pgm 
+        fileChooser.setAcceptAllFileFilterUsed(true); // remove all files option
+        // Adjust size of file chooser popup
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
         fileChooser.setPreferredSize(new Dimension((int)(dim.width*.6),(int)(dim.height*.8)));
-        int returnVal = fileChooser.showDialog(this, "Open image");
+
+        int returnVal = fileChooser.showDialog(this, "Load images");
         if (returnVal==0) {
             this.dispose();
-            return fileChooser.getSelectedFile().getPath();
+            return fileChooser.getSelectedFiles();
         }else{
             this.dispose();
             return null;
