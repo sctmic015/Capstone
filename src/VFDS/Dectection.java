ff6a0ea8f8e34a02b485c292beb610438366f8ab
sctmic015 <michaelscott.scott77@gmail.com>
2021-10-03 20:14:16 +0200

    Thresholds and tentacles. Ify with the smaller data sets and do not know why

--- src/VFDS/Dectection.java
+++ src/VFDS/Dectection.java
@@ -100,25 +100,77 @@ public class Dectection {
      */
     public static ArrayList<FractureVoxel> findFractureVoxels(CTImageSlice imageSlice){
         ArrayList<FractureVoxel> fractureVoxels = new ArrayList<FractureVoxel>();
-        for (int x = 1; x < imageSlice.getXDimension() -1; x ++){
-            for (int y = 1; y < imageSlice.getYDimension() -1; y ++){
+        for (int x = 5; x < imageSlice.getXDimension() -5; x ++){
+            for (int y = 5; y < imageSlice.getYDimension() -5; y ++){
+                System.out.println(LOWER_BOUND_THRESHOLD + " " + UPPER_BOUND_THRESHOLD);
                 int voxelValue = imageSlice.getImageData()[x][y];
                 // if (voxelValue > 0 && voxelValue < 10) {
                 //     System.out.println(voxelValue);
                 // }
-                if (voxelValue > LOWER_BOUND_THRESHOLD &&
-                    voxelValue < UPPER_BOUND_THRESHOLD
+                if (tentacles(imageSlice, x, y) == 0){
+                }
+                else if (tentacles(imageSlice, x, y) == 1){
+                    if (voxelValue > LOWER_BOUND_THRESHOLD &&
+                            voxelValue <= UPPER_BOUND_THRESHOLD
                     ){
                         fractureVoxels.add(new FractureVoxel(imageSlice.getZCoOrd(), y, x));
                         //System.out.print(imageData[x][y]);
-                        // System.out.println("X-coOrd: " + y + " Y-coOrd: " + x);  // For some reason x and y are confused lol
+                        //System.out.println("X-coOrd: " + y + " Y-coOrd: " + x);  // For some reason x and y are confused lol
                     }
+                }
+                else{
+                    if (voxelValue <= UPPER_BOUND_THRESHOLD
+                    ){
+                        fractureVoxels.add(new FractureVoxel(imageSlice.getZCoOrd(), y, x));
+                        //System.out.print(imageData[x][y]);
+                       // System.out.println("X-coOrd: " + y + " Y-coOrd: " + x);  // For some reason x and y are confused lol
+                    }
+                }
+
+
             }
             //System.out.println();
         }
         return fractureVoxels;
     }
 
+    public static int tentacles(CTImageSlice imageSlice, int x, int y){
+        boolean inShape = false;
+        int count = 0;
+        // Tentacle one: left side
+        for (int i = x-3; i < x; i ++){
+            if (imageSlice.getImageData()[i][y] > LOWER_BOUND_THRESHOLD) {
+                count ++;
+                break;
+            }
+        }
+        for (int i = x+1; i < x +4; i ++){
+            if (imageSlice.getImageData()[i][y] > LOWER_BOUND_THRESHOLD) {
+                count ++;
+                break;
+            }
+        }
+        for (int i = y-3; i < y; i ++){
+            if (imageSlice.getImageData()[x][i] > LOWER_BOUND_THRESHOLD) {
+                count ++;
+                break;
+            }
+        }
+        for (int i = y+1; i < y +4; i ++){
+            if (imageSlice.getImageData()[x][i] > LOWER_BOUND_THRESHOLD) {
+                count ++;
+                break;
+            }
+        }
+        if (count == 0 || count == 1){
+            return 0;
+        }
+        if (count == 2) {
+            return 1;
+        }
+        else
+            return 2;
+    }
     /** 
      * Method to test the number of coloured voxels around a fractured voxel
      * If the count is greater than 4 we can assume it is a fracture
