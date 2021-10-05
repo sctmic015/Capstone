b8b211889c9cea1d63b21c2f5bfc1235d806f2fb
DavidBlore <davidblore6555@gmail.com>
2021-10-05 18:15:42 +0200

    not working

--- src/VFDS/ForkJoinReadInFile.java
+++ src/VFDS/ForkJoinReadInFile.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 
 
 public class ForkJoinReadInFile extends RecursiveAction{
-    private static int threshold = 25; // threshold for recursion (num. of images to work on per a thread)
+    private static int threshold = 10; // threshold for recursion (num. of images to work on per a thread)
 
     private ArrayList<File> files;
     private ArrayList<CTImageSlice> imageSlices;
@@ -32,18 +32,34 @@ public class ForkJoinReadInFile extends RecursiveAction{
     protected void compute() {
         // https://docs.oracle.com/javase/tutorial/essential/concurrency/forkjoin.html
         // https://mkyong.com/java/java-fork-join-framework-examples/
-        if (imageSlices.size() <= threshold) {
+        // to make seq. change "files" to "imageSlices" 
+        int fsize=files.size(); //TODO:remove
+        if (files.size() <= threshold) {
             // compute directly
-            for (int i = 0; i < files.size(); i++) {
+            for (int i = 0; i < files.size()-1; i++) {
                 imageSlices.add(start+i, new CTImageSlice(i,files.get(i)) );
             }
         }else{
-            // TODO: check that this captures all the images? i.e., doesnt 'miss' images
-            int split = files.size()/2;
-            invokeAll(
-                new ForkJoinReadInFile(imageSlices, (ArrayList<File>)files.subList(start, split), start),
-                new ForkJoinReadInFile(imageSlices, (ArrayList<File>)files.subList(start+split, files.size()), start+split)
-            );
+            if (start==files.size()) {
+                // TODO: is this ok?
+                // imageSlices.add(start-1, new CTImageSlice(start,files.get(start)) );
+            }else{
+                // TODO: check that this captures all the images? i.e., doesnt 'miss' images
+                int split = files.size()/2;
+                ArrayList<File> files1=new ArrayList<File>();
+                ArrayList<File> files2=new ArrayList<File>();
+                try {
+                    files1.addAll(files.subList(start, start+split));
+                    files2.addAll(files.subList(start+split, files.size()));
+                } catch (Exception e) {
+                    //TODO: handle exception
+                    System.out.println();
+                }
+                invokeAll(
+                    new ForkJoinReadInFile(imageSlices, files1, start),
+                    new ForkJoinReadInFile(imageSlices, files2, start+split)
+                );
+            }
         }
     }
     
